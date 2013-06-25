8a59a803d27e95140abcbdda126ae11206f9f629
Ryan Stocker <stocker.19@osu.edu>
2013-06-25 16:14:43 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raven::Application.config.secret_key_base = '5d73f8a20fc0744124bdec6aeb7de8d835dc2e78f43726085147ee482ae9add35312872d4145affbb76b21aa6e9e9201b1970b5250d53e9a50d0edfabed220fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Raven::Application.config.session_store :cookie_store, key: '_raven_session'
