99d5559641b7d36d5d04e4f921a7820c1146edfd
Ryan Stocker <stocker.19@osu.edu>
2013-08-01 14:24:14 -0400

    relationships

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ActiveRecord::Base
+  has_many :ravens
 
+  encrypt_with_public_key :title, key_pair: Rails.root.join('config','keypair.pem')
+  encrypt_with_public_key :body, key_pair: Rails.root.join('config','keypair.pem')
 end
